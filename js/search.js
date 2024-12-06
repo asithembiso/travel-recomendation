4e101180922e5e2e4ddbc56bc1e6edf7715b20be
Athi Sithembiso <asithembiso@gmail.com>
2024-12-06 09:51:01 +0200

    fix

--- js/search.js
+++ js/search.js
@@ -8,24 +8,23 @@ function searchCondition() {
     fetch('travel_api.json')
         .then(response => response.json())
         .then(data => {
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+        const country = data.countries.find(item => item.name.toLowerCase() === input);
 
-            if (condition) {
-                const symptoms = condition.symptoms.join(', ');
-                const prevention = condition.prevention.join(', ');
-                const treatment = condition.treatment;
+            if (country) {
+                array = country.cities
+                array.forEach(city => {
+                    resultDiv.innerHTML += `<div class="custom-card">`;
+                    resultDiv.innerHTML += `<img src="${city.imageUrl}" alt="image" class="w-full h-40 object-cover">`;
+                    resultDiv.innerHTML += `<div class="p-4">`;
+                    resultDiv.innerHTML += `<h3 class="text-xl font-bold mb-2">${city.name}</h3></p>`;
+                    resultDiv.innerHTML += `<p class="text-gray-600 mb-3 text-sm">${city.description}</p>`;
+                    resultDiv.innerHTML += `<a href="#" class="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md text-sm transition">Visit Service</a>">`;
+                    resultDiv.innerHTML += "</div></div>"
+                });
 
-                resultDiv.innerHTML += `<div class="custom-card">`;
-                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="image" class="w-full h-40 object-cover">`;
-
-                resultDiv.innerHTML += `<div class="p-4">`;
-                resultDiv.innerHTML += `<h3 class="text-xl font-bold mb-2">Service One</h3></p>`;
-                resultDiv.innerHTML += `<p class="text-gray-600 mb-3 text-sm">Innovative solutions for your business needs.</p>`;
-                resultDiv.innerHTML += `<a href="#" class="inline-block bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md text-sm transition">Visit Service</a>">`;
-                resultDiv.innerHTML += "</div></div>"
 
             } else {
-                resultDiv.innerHTML = 'Condition not found.';
+                resultDiv.innerHTML = 'Country not found.';
             }
         })
         .catch(error => {
